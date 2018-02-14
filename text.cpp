5c9d7cb989d78df05e3019ca7f844f1c93b030be
Reedz22 <Reedz22@users.noreply.github.com>
2018-02-14 11:55:59 -0400

    Lab3 commit

---
+++ text.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+	ofstream out {"abc.txt"};
+	if (out)
+	{
+		out<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+		cout<<endl<<endl;
+	}
+	else
+	{
+		cout << "could not read";
+	}
+ 
+   return 0;
+}
